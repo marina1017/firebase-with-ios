c5bf582e69591d2a4ffc6a17e4c9af2742c8c6a9
marina1017 <mari10172001@yahoo.co.jp>
2018-04-11 23:22:57 +0900

    LoginViewControllerの動作チェック

--- starter/ToDo App/ToDo App/LoginViewController.swift
+++ starter/ToDo App/ToDo App/LoginViewController.swift
@@ -7,22 +7,95 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //FIRAuthは→Authにする
+        if let _ = Auth.auth().currentUser {
+            self.signIn()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func didTapSignIn(_ sender: UIButton) {
-        
+        let email = emailField.text
+        let password = passwordField.text
+        Auth.auth().signIn(withEmail: email!, password: password!, completion: { (user, error) in
+            guard let _ = user else {
+                if let error = error {
+                    if let errCode = AuthErrorCode(rawValue: error._code) {
+                        //ここ公式と異なる 参考(https://code.i-harness.com/ja/q/23b70bf)
+                        switch errCode {
+                        case .invalidEmail:
+                            self.showAlert("User account not found. Try registering")
+                        case .wrongPassword:
+                            self.showAlert("Incorrect username/password combination")
+                        default:
+                            self.showAlert("Error:\(error.localizedDescription)")
+                        }
+                    }
+                    return
+                }
+                //このreturnがいる
+                return assertionFailure("user and error are nil")
+            }
+            self.signIn()
+        })
     }
     
     @IBAction func didRequestPasswordReset(_ sender: UIButton) {
+        let prompt = UIAlertController(title: "To Do App", message: "Email:", preferredStyle: .alert)
+        let okAciont = UIAlertAction(title: "OK", style: .default) { _ in
+            let userInput = prompt.textFields![0].text
+            if (userInput!.isEmpty) {
+                return
+            }
+            Auth.auth().sendPasswordReset(withEmail: userInput!, completion: { (error) in
+                if let error = error {
+                    if let errCode = AuthErrorCode(rawValue: error._code) {
+                        switch errCode {
+                        case .userNotFound:
+                            DispatchQueue.main.async{
+                                self.showAlert("User account not found. Try registering")
+                            }
+                        default:
+                            DispatchQueue.main.async {
+                                self.showAlert("Error: \(error.localizedDescription)")
+                            }
+                        }
+                    }
+                    return
+                } else {
+                    DispatchQueue.main.async {
+                        self.showAlert("You'll receive an email shortly to reset your password.")
+                    }
+                }
+                
+            })
+        }
+        prompt.addTextField(configurationHandler: nil)
+        prompt.addAction(okAciont)
+        self.present(prompt, animated: true, completion: nil)
         
+    }
+    
+    func showAlert(_ message: String) {
+        let alertController = UIAlertController(title: "To Do App", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func signIn() {
+        self.performSegue(withIdentifier: "SignInFromLogin", sender: nil)
     }
     
 }
